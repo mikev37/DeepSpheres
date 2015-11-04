cbb1969357bb1dd208c0c5132634a4597a9e8f43
Karl Justice <Karl Justice>
2015-11-04 10:07:05 -0500

    Added start of ScriptParser, with PDFMiner included

---
+++ ScriptParser/ScriptParse.py
@@ -0,0 +1,67 @@
+'''
+Created on Nov 2, 2015
+
+@author: karl
+'''
+#!/usr/bin/python
+import _mysql
+
+from pdfminer.pdfparser import PDFParser
+from pdfminer.pdfdocument import PDFDocument
+from pdfminer.pdfpage import PDFPage
+from pdfminer.pdfpage import PDFTextExtractionNotAllowed
+from pdfminer.pdfinterp import PDFResourceManager
+from pdfminer.pdfinterp import PDFPageInterpreter
+from pdfminer.pdfdevice import PDFDevice
+from pdfminer.layout import LAParams
+from pdfminer.converter import PDFPageAggregator
+from ccm.Pages import Page
+
+ 
+   
+
+
+def GetScript(self, filename):
+    password = ""
+    filename = "sources/Analyze_That.pdf"
+
+def GetScript(self, filename):
+    password = ""
+    filename = "sources/Analyze_That.pdf"
+    # Open a PDF file.
+    fp = open(filename, 'rb')
+    # Create a PDF parser object associated with the file object.
+    parser = PDFParser(fp)
+    # Create a PDF document object that stores the document structure.
+    # Supply the password for initialization.
+    document = PDFDocument(parser, password)
+    # Check if the document allows text extraction. If not, abort.
+    if not document.is_extractable:
+        raise PDFTextExtractionNotAllowed
+    # Create a PDF resource manager object that stores shared resources.
+    rsrcmgr = PDFResourceManager()
+    # Create a PDF device object.
+    device = PDFDevice(rsrcmgr)
+    # Create a PDF interpreter object.
+    interpreter = PDFPageInterpreter(rsrcmgr, device)
+    # Process each page contained in the document.
+    for page in PDFPage.create_pages(document):
+        interpreter.process_page(page)
+    
+    # Set parameters for analysis.
+    laparams = LAParams()
+    laparams.boxes_flow = 1.0
+    # Create a PDF page aggregator object.
+    device = PDFPageAggregator(rsrcmgr, laparams=laparams)
+    interpreter = PDFPageInterpreter(rsrcmgr, device)
+    for page in PDFPage.create_pages(document):
+        interpreter.process_page(page)
+        # receive the LTPage object for the page.
+        layout = device.get_result()
+        for page in layout:
+            try:
+                print page.x0
+                print page.get_text()
+            except:
+                temp=5
+    return
